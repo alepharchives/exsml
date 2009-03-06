978fc6f671b4a8bb8206ffdff2baddd081eeb1aa
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-03-06 02:24:23 +0100

    Indentation.

--- runtime/fail.c
+++ runtime/fail.c
@@ -19,49 +19,50 @@ volatile int float_exn = SYS__EXN_FAIL;
 
 double maxdouble = DBL_MAX/2;
 
-struct longjmp_buffer * external_raise;
+struct longjmp_buffer *external_raise;
 value exn_bucket;		/* ML type: string ref * 'a */
 
-EXTERN void mlraise(value v)
+extern void mlraise(value v)
 {
-  in_blocking_section = 0;
-  exn_bucket = v;
-  longjmp(external_raise->buf, 1);
+	in_blocking_section = 0;
+	exn_bucket = v;
+	longjmp(external_raise->buf, 1);
 }
 
 /* Raise a unary pervasive exception with the given argument */
 
 void raiseprimitive1(int exnindex, value arg) {
-  value exn;
-  Push_roots(r, 1);
-  r[0] = arg;
-  exn = alloc_tuple(2);
-  modify(&Field(exn, 0), Field(global_data, exnindex));
-  modify(&Field(exn, 1), r[0]);
-  Pop_roots();
-  mlraise(exn);
+	value exn;
+
+	Push_roots(r, 1);
+	r[0] = arg;
+	exn = alloc_tuple(2);
+	modify(&Field(exn, 0), Field(global_data, exnindex));
+	modify(&Field(exn, 1), r[0]);
+	Pop_roots();
+	mlraise(exn);
 }
 
 void raiseprimitive0(int exnindex) {
-  raiseprimitive1(exnindex, Val_unit);
+	raiseprimitive1(exnindex, Val_unit);
 }
 
-EXTERN void raise_with_string(int exnindex, char * msg) {
-  raiseprimitive1(exnindex, copy_string(msg));
+extern void raise_with_string(int exnindex, char * msg) {
+	raiseprimitive1(exnindex, copy_string(msg));
 }
 
-EXTERN void failwith (char* msg) {
-  raise_with_string(SYS__EXN_FAIL, msg);
+extern void failwith (char* msg) {
+	raise_with_string(SYS__EXN_FAIL, msg);
 }
 
 void invalid_argument (char * msg) {
-  raise_with_string(SYS__EXN_ARGUMENT, msg);
+	raise_with_string(SYS__EXN_ARGUMENT, msg);
 }
 
 void raise_out_of_memory() {
-  raiseprimitive0(SYS__EXN_MEMORY);
+	raiseprimitive0(SYS__EXN_MEMORY);
 }
 
 void raise_overflow() {
-  raiseprimitive0(SYS__EXN_OVERFLOW);
+	raiseprimitive0(SYS__EXN_OVERFLOW);
 }
