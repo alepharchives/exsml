d80fcf2440f56fa9015672663b94aea1351c9799
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-03-07 16:03:39 +0100

    Make Coccinelle able to parse the callback interface. Also reindent.

--- runtime/callback.c
+++ runtime/callback.c
@@ -20,66 +20,73 @@ static valueptr reg_cptr_ptr_         = (valueptr)NULL;
 /* Obtain an ML value pointer from a string.  Return NULL if the name
    is not registered, or has been unregistered. */
 
-valueptr get_valueptr(char* nam) {
-  /* opt is an ML option type */
-  value opt = callback(get_valueptr_, copy_string(nam));
-  if (opt == NONE) {
-    return (valueptr)NULL;		/* Not an ML value     */
-  } else
-    return (valueptr)(Field(opt, 0));   /* res = SOME valueptr */
+valueptr get_valueptr(char* nam)
+{
+	/* opt is an ML option type */
+	value opt = callback(get_valueptr_, copy_string(nam));
+	if (opt == NONE) {
+		return (valueptr)NULL;		/* Not an ML value     */
+	} else
+		return (valueptr)(Field(opt, 0));   /* res = SOME valueptr */
 }
 
 /* Obtain an ML value from a value pointer.  Fail if the value pointer
    is NULL.  Return NULL if the value has been unregistered. */
 
-value get_value(valueptr mvp) {
-  value opt;
-  if (mvp == (valueptr)NULL)
-    failwith("get_value: null ML value pointer");
-  opt = Field(mvp, 0);
-  if (opt == NONE)
-    return (value)NULL;	        /* Not an ML value */
-  else
-    return (value)(Field(opt, 0));	/* opt == SOME v   */
+value get_value(valueptr mvp)
+{
+	value opt;
+	if (mvp == (valueptr)NULL)
+		failwith("get_value: null ML value pointer");
+	opt = Field(mvp, 0);
+	if (opt == NONE)
+		return (value)NULL;	        /* Not an ML value */
+	else
+		return (value)(Field(opt, 0));	/* opt == SOME v   */
 }
 
-value callbackptr(valueptr closureptr, value arg1) {
-  value closure = get_value(closureptr);
-  if (closure == (value)NULL)
-    failwith("callbackptr: ML value has been unregistered");
-  return callback(closure, arg1);
+value callbackptr(valueptr closureptr, value arg1)
+{
+	value closure = get_value(closureptr);
+	if (closure == (value)NULL)
+		failwith("callbackptr: ML value has been unregistered");
+	return callback(closure, arg1);
 }
 
-value callbackptr2(valueptr closureptr, value arg1, value arg2) {
-  value closure = get_value(closureptr);
-  if (closure == (value)NULL)
-    failwith("callbackptr2: ML value has been unregistered");
-  return callback2(closure, arg1, arg2);
+value callbackptr2(valueptr closureptr, value arg1, value arg2)
+{
+	value closure = get_value(closureptr);
+	if (closure == (value)NULL)
+		failwith("callbackptr2: ML value has been unregistered");
+	return callback2(closure, arg1, arg2);
 }
 
-value callbackptr3(valueptr closureptr, value arg1, value arg2, value arg3) {
-  value closure = get_value(closureptr);
-  if (closure == (value)NULL)
-    failwith("callbackptr3: ML value has been unregistered");
-  return callback3(closure, arg1, arg2, arg3);
+value callbackptr3(valueptr closureptr, value arg1, value arg2, value arg3)
+{
+	value closure = get_value(closureptr);
+	if (closure == (value)NULL)
+		failwith("callbackptr3: ML value has been unregistered");
+	return callback3(closure, arg1, arg2, arg3);
 }
 
 /* This calls Callback.register */
 
-void registervalue(char* nam, value mlval) {
-  value namval;
-  Push_roots(r, 1);
-  r[0] = mlval;
-  namval = copy_string(nam);
-  callback2(get_value(reg_mlvalueptr_ptr_), namval, r[0]);
-  Pop_roots();
+void registervalue(char* nam, value mlval)
+{
+	value namval;
+	Push_roots(r, 1);
+	r[0] = mlval;
+	namval = copy_string(nam);
+	callback2(get_value(reg_mlvalueptr_ptr_), namval, r[0]);
+	Pop_roots();
 }
 
 /* This calls Callback.unregister */
 
-void unregistervalue(char* nam) {
-  value namval = copy_string(nam);
-  callback(get_value(unreg_mlvalueptr_ptr_), namval);
+void unregistervalue(char* nam)
+{
+	value namval = copy_string(nam);
+	callback(get_value(unreg_mlvalueptr_ptr_), namval);
 }
 
 /* Allocate a reference cell in the old heap, so it will not be moved.
@@ -87,20 +94,21 @@ void unregistervalue(char* nam) {
 
 valueptr alloc_valueptr(value v) /* ML */
 {
-  value res;
-  Push_roots(r, 1);
-  r[0] = v;
-  res = alloc_shr (1, Reference_tag); // An 'a ref
-  initialize(&Field(res, 0), r[0]);
-  Pop_roots();
-  return res;
+	value res;
+	Push_roots(r, 1);
+	r[0] = v;
+	res = alloc_shr (1, Reference_tag); // An 'a ref
+	initialize(&Field(res, 0), r[0]);
+	Pop_roots();
+	return res;
 }
 
-void registercptr(char* nam, void* cptr) {
-  // A simple way to initialize the ML value pointer once
-  if (reg_cptr_ptr_ == (valueptr)NULL)
-    reg_cptr_ptr_ = get_valueptr("Callback.registercptr");
-  callbackptr2(reg_cptr_ptr_, copy_string(nam), (value)cptr);
+void registercptr(char* nam, void* cptr)
+{
+	// A simple way to initialize the ML value pointer once
+	if (reg_cptr_ptr_ == (valueptr)NULL)
+		reg_cptr_ptr_ = get_valueptr("Callback.registercptr");
+	callbackptr2(reg_cptr_ptr_, copy_string(nam), (value)cptr);
 }
 
 /* Initialization.  This is to be called from the ML side when the
@@ -111,72 +119,73 @@ void registercptr(char* nam, void* cptr) {
 
 value sml_init_register(value v)	/* ML */
 {
-  /* The closure in v may be moved if it is not in the old heap.  We
-     force it into the old heap by requesting a minor collection: */
-  Push_roots(r, 1);
-  r[0] = v;
-  minor_collection();
-  get_valueptr_         = r[0];
-  Pop_roots();
-  reg_mlvalueptr_ptr_   = get_valueptr("Callback.register");
-  unreg_mlvalueptr_ptr_ = get_valueptr("Callback.unregister");
-  return Val_unit;
+	/* The closure in v may be moved if it is not in the old heap.  We
+	   force it into the old heap by requesting a minor collection: */
+	Push_roots(r, 1);
+	r[0] = v;
+	minor_collection();
+	get_valueptr_         = r[0];
+	Pop_roots();
+	reg_mlvalueptr_ptr_   = get_valueptr("Callback.register");
+	unreg_mlvalueptr_ptr_ = get_valueptr("Callback.unregister");
+	return Val_unit;
 }
 
 /* Accessing C variables and functions from ML.  Used by Dynlib and Callback */
 
 value c_var(value symhdl) /* ML */
 {
-  return *((value *) symhdl);
+	return *((value *) symhdl);
 }
 
 value cfun_app1(value cfun, value arg)  /* ML */
 {
-  /* Due to the heavy typecasting, I had to declare a temp variable in
-     order to get it right.
-  */
-  value (*cp)(value) = (value (*)(value)) cfun;
+	/* Due to the heavy typecasting, I had to declare a temp variable in
+	   order to get it right.
+	*/
+	value (*cp)(value) = (long (*)(long)) cfun;
 
-  return (*cp)(arg);
+	return (*cp)(arg);
 }
 
 value cfun_app2(value cfun, value arg1, value arg2)  /* ML */
 {
-  /* again a typecast value */
-  value (*cp)(value,value) = (value (*)(value,value)) cfun;
+	/* again a typecast value */
+	value (*cp)(value,value) = (long (*)(long,long)) cfun;
 
-  return (*cp)(arg1,arg2);
+	return (*cp)(arg1,arg2);
 }
 
 value cfun_app3(value cfun, value arg1, value arg2, value arg3)  /* ML */
 {
-  /* again a typecast value */
-  value (*cp)(value,value,value) = (value (*)(value,value,value)) cfun;
+	/* again a typecast value */
+	value (*cp)(value,value,value) = (long (*)(long,long,long)) cfun;
 
-  return (*cp)(arg1,arg2,arg3);
+	return (*cp)(arg1,arg2,arg3);
 }
 
 value cfun_app4(value cfun, value arg1, value arg2, value arg3, value arg4)  /* ML */
 {
-  /* again a typecast value */
-  value (*cp)(value,value,value,value) =
-    (value (*)(value,value,value,value)) cfun;
+	/* again a typecast value */
+	value (*cp)(value,value,value,value);
 
-  return (*cp)(arg1,arg2,arg3,arg4);
+	cp = (long (*)(long,long,long,long)) cfun;
+
+	return cp(arg1,arg2,arg3,arg4);
 }
 
 value cfun_app5(value args, int argc)  /* ML */
 {
-  value cfun = Field(args, 0);
-  value arg1 = Field(args, 1);
-  value arg2 = Field(args, 2);
-  value arg3 = Field(args, 3);
-  value arg4 = Field(args, 4);
-  value arg5 = Field(args, 5);
+	value cfun = Field(args, 0);
+	value arg1 = Field(args, 1);
+	value arg2 = Field(args, 2);
+	value arg3 = Field(args, 3);
+	value arg4 = Field(args, 4);
+	value arg5 = Field(args, 5);
 
-  /* again a typecast value */
-  value (*cp)(value,value,value,value,value) =
-    (value (*)(value,value,value,value,value)) cfun;
+	/* again a typecast value */
+	value (*cp)(value,value,value,value,value) =
+		(long (*)(long,long,long,long,long)) cfun;
 
-  return (*cp)(arg1,arg2,arg3,arg4,arg5);
+	return (*cp)(arg1,arg2,arg3,arg4,arg5);
 }
