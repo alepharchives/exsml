69c765f7a361d43300125126d07b46b83d216f8e
Alexander Færøy <ahf@0x90.dk>
2009-03-09 10:12:49 +0100

    Bail out if something goes wrong in autogen.sh. Add --copy --force --automake as arguments to libtoolize and add libtool to configure.ac.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-libtoolize
-aclocal
-autoheader
-autoconf
-automake -a --copy
+run() {
+    echo ">>> $@" 1>&2
+
+    if ! $@ ; then
+        echo "Error!" 1>&2
+        exit 1
+    fi
+}
+
+run libtoolize --copy --force --automake
+run aclocal
+run autoheader
+run autoconf
+run automake -a --copy
