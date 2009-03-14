6e04d2d635c122b637254bbebc0c9647df33de77
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-03-14 19:37:30 +0100

    Cleanup the basic debugger code.

--- runtime/debugger.c
+++ runtime/debugger.c
@@ -11,6 +11,8 @@
 #include "stacks.h"
 #include "unalignd.h"
 
+extern bytecode_t start_code;
+
 bytecode_t log_buffer[LOG_BUFFER_SIZE];
 bytecode_t * log_ptr;
 int trace_flag;
@@ -22,73 +24,75 @@ long max_print_depth = 10;
 
 long print_cnt;
 
-static void print_val(v, d)
-     value v;
-     long d;
+static void print_val(value v, long d)
 {
-  long n;
-  value * p;
+	long n;
+	value * p;
 
-  if (d <= 0) {
-    printf(".");
-    return;
-  }
-  print_cnt--;
-  if (print_cnt <= 0) {
-    if (print_cnt == 0) printf("...");
-    return;
-  }
-  if (Is_long(v))
-    printf("%ld", Long_val(v));
-  else if (!Is_in_heap (v) && !Is_young (v))
-    printf("0x%lx", v);
-  else switch(Tag_val(v)) {
-    case String_tag:
-      printf("\"%s\"", String_val(v));
-      break;
-    case Double_tag:
-      printf("%g", Double_val(v));
-      break;
-    case Abstract_tag:
-      printf("<abstract>");
-      break;
-    case Final_tag:
-      printf("<finalized>");
-      break;
-    default:
-      n = Tag_val(v);
-      if (n < 26) {
-	      char c = (char) n + 'A';
-	      printf ("%c", c);
-      }else{
-        printf("tag%ld", n);
-      }
-      n = Wosize_val(v);
-      if (n > 0) {
-        printf("(");
-        p = &Field(v, 0);
-        while (n > 1) {
-          print_val(*p, d-1);
-          printf(", ");
-          p++;
-          n--;
-        }
-        print_val(*p, d-1);
-        printf(")");
-      }
-      break;
-  }
+	if (d <= 0) {
+		printf(".");
+		return;
+	}
+
+	print_cnt--;
+
+	if (print_cnt <= 0) {
+		if (print_cnt == 0) printf("...");
+		return;
+	}
+
+	if (Is_long(v)) {
+		printf("%ld", Long_val(v));
+	} else if (!Is_in_heap (v) && !Is_young (v)) {
+		printf("0x%lx", v);
+	} else {
+		switch(Tag_val(v)) {
+		case String_tag:
+			printf("\"%s\"", String_val(v));
+			break;
+		case Double_tag:
+			printf("%g", Double_val(v));
+			break;
+		case Abstract_tag:
+			printf("<abstract>");
+			break;
+		case Final_tag:
+			printf("<finalized>");
+			break;
+		default:
+			n = Tag_val(v);
+			if (n < 26) {
+				char c = (char) n + 'A';
+				printf ("%c", c);
+			}else{
+				printf("tag%ld", n);
+			}
+			n = Wosize_val(v);
+			if (n > 0) {
+				printf("(");
+				p = &Field(v, 0);
+				while (n > 1) {
+					print_val(*p, d-1);
+					printf(", ");
+					p++;
+					n--;
+				}
+				print_val(*p, d-1);
+				printf(")");
+			}
+			break;
+		}
+	}
 }
 
-void print_value(v)
-	value v;
+void print_value(value v)
 {
-  print_cnt = max_print;
-  print_val(v, max_print_depth);
-  printf("\n");
+	print_cnt = max_print;
+	print_val(v, max_print_depth);
+	printf("\n");
 }
 
-extern bytecode_t start_code;
+
 
 void print_pc(pc)
      bytecode_t pc;
@@ -103,53 +107,50 @@ void print_pc(pc)
 
 /* Disassembling one instruction */
 
-bytecode_t disasm_instr(pc)
-	bytecode_t pc;
+bytecode_t disasm_instr(bytecode_t pc)
 {
-  return pc;
+	return pc;
 }
 
-void disasm(pc)
-     bytecode_t pc;
+void disasm(bytecode_t pc)
 {
-  int i;
+	int i;
 
-  for (i = 0; i < 20; i++)
-    pc = disasm_instr(pc);
+	for (i = 0; i < 20; i++) {
+		pc = disasm_instr(pc);
+	}
 }
 
-void post_mortem(n)
-	int n;
+void post_mortem(int n)
 {
-  bytecode_t * p;
+	bytecode_t * p;
 
-  if (n > LOG_BUFFER_SIZE) n = LOG_BUFFER_SIZE;
-  for (p = log_buffer +
-             (unsigned) (log_ptr - log_buffer - n) % LOG_BUFFER_SIZE;
-       n > 0;
-       n--) {
-    disasm_instr(*p);
-    p++;
-    if (p >= log_buffer + LOG_BUFFER_SIZE) p = log_buffer;
-  }
+	if (n > LOG_BUFFER_SIZE) n = LOG_BUFFER_SIZE;
+	for (p = log_buffer +
+		     (unsigned) (log_ptr - log_buffer - n) % LOG_BUFFER_SIZE;
+	     n > 0;
+	     n--) {
+		disasm_instr(*p);
+		p++;
+		if (p >= log_buffer + LOG_BUFFER_SIZE) {
+			p = log_buffer;
+		}
+	}
 }
 
-void failed_assert (expr, file, line)
-     char *expr, *file;
-     int line;
+void failed_assert (char *expr, char *file, int line)
 {
-  fprintf (stderr, "Assertion failed: %s; file %s; line %d\n",
-	   expr, file, line);
-  exit (100);
+	fprintf (stderr, "Assertion failed: %s; file %s; line %d\n",
+		 expr, file, line);
+	exit (100);
 }
 
 static unsigned long seed = 0x12345;
 
 unsigned long not_random ()
 {
-  seed = seed * 65537 + 12345;
-  return seed;
+	seed = seed * 65537 + 12345;
+	return seed;
 }
-
 
 #endif /* DEBUG */
