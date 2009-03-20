e9bb3328a9098eb521aa70d68d33874031d9f349
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-03-20 01:05:01 +0100

    Cleanup round on dynlib.c.

--- runtime/dynlib.c
+++ runtime/dynlib.c
@@ -5,17 +5,22 @@
 #include "mlvalues.h"
 #include "fail.h"
 
+value dynlib_dlopen(value, value);
+value dynlib_dlclose(value);
+value dynlib_dlsym(value, value);
+
 /* Ken Larsen (kla@it.dtu.dk) 1998-01-08 */
 value dynlib_dlopen(value libname, value flagval) /* ML */
 {
 	void *handle;
 
-	int mlflags = VAL_TO_LONG(flagval);
+	long mlflags = VAL_TO_LONG(flagval);
 	int cflags;
-	if (1 & mlflags)
+	if (1 & mlflags) {
 		cflags = RTLD_NOW;
-	else
+	} else {
 		cflags = RTLD_LAZY;
+	}
 
 	handle = dlopen (String_val(libname), cflags);
 	if (!handle) {
@@ -26,22 +31,20 @@ value dynlib_dlopen(value libname, value flagval) /* ML */
 	return (value) handle;
 }
 
-value dynlib_dlclose(value handle) /* ML */
+value dynlib_dlclose(value handle)
 {
   dlclose((void *) handle);
 
   return Val_unit;
 }
 
-value dynlib_dlsym(value handle, value sym) /* ML */
+value dynlib_dlsym(value handle, value sym)
 {
-  void *symhdl;
+  void *symhdl = dlsym((void *) handle, String_val(sym));
+  char *error = dlerror();
 
-  symhdl = dlsym((void *) handle, String_val(sym));
-  {
-    char *error = dlerror();
-    if (error != NULL)
-      failwith(error);
+  if (error != NULL) {
+	  failwith(error);
   }
 
   return (value) symhdl;
