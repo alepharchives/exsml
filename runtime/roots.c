6b4c02df9e039b94078559caa1f5330b6f8c68c5
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-03-24 16:25:17 +0100

    Reindent roots.c

--- runtime/roots.c
+++ runtime/roots.c
@@ -6,23 +6,19 @@
 #include "mlvalues.h"
 #include "stacks.h"
 
-void local_roots (copy_fn)
-     void (*copy_fn) ();
+void local_roots (void (*copy_fn) ())
 {
-  value *sp;
+	value *sp, *block;
 
-  /* stack */
-  for (sp = extern_sp; sp < stack_high; sp++) {
-    copy_fn (sp, *sp);
-  }
+	/* stack */
+	for (sp = extern_sp; sp < stack_high; sp++) {
+		copy_fn (sp, *sp);
+	}
 
-  /* C roots */
-  {
-    value *block;
-    for (block = c_roots_head; block != NULL; block = (value *) block [1]){
-      for (sp = block - (long) block [0]; sp < block; sp++){
-	copy_fn (sp, *sp);
-      }
-    }
-  }
+	/* C roots */
+	for (block = c_roots_head; block != NULL; block = (value *) block [1]) {
+		for (sp = block - (long) block [0]; sp < block; sp++) {
+			copy_fn (sp, *sp);
+		}
+	}
 }
