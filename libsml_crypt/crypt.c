7bea77a3dae77a652e7984e5646a6e5676dccc53
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-04-01 19:08:26 +0200

    Fix prototypes.

--- libsml_crypt/crypt.c
+++ libsml_crypt/crypt.c
@@ -2,6 +2,7 @@
    The C side of things: how to define the C function to be invoked from SML
  */
 
+#define _XOPEN_SOURCE
 #include <unistd.h>		/* For crypt       */
 
 /* Moscow ML specific includes: */
@@ -9,18 +10,20 @@
 #include <mlvalues.h>		/* For String_val  */
 #include <alloc.h>		/* For copy_string */
 
+value ml_crypt(value, value);
+
 /* Type on the SML side: string -> string -> string */
 
 value ml_crypt(value mlkey, value mlsalt)
 {
-  /* Obtain pointers to the SML strings mlkey and mlsalt in the SML heap: */
-  char *key  = String_val(mlkey);
-  char *salt = String_val(mlsalt);
+	/* Obtain pointers to the SML strings mlkey and mlsalt in the SML heap: */
+	char *key  = String_val(mlkey);
+	char *salt = String_val(mlsalt);
 
-  /* Invoke C library function: */
-  char *res = (char*)crypt(key, salt);
+	/* Invoke C library function: */
+	char *res = (char*)crypt(key, salt);
 
-  /* The result is a pointer to a null-terminated C string.    */
-  /* Copy it to a fresh string in the ML heap and return that: */
-  return copy_string(res);
+	/* The result is a pointer to a null-terminated C string.    */
+	/* Copy it to a fresh string in the ML heap and return that: */
+	return copy_string(res);
 }
